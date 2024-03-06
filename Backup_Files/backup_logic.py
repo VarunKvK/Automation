20731044910229d892e834a305a388ccdf7b0b04
VarunKvK <varunkrishnankv2002@gmail.com>
2024-03-06 09:14:02 +0530

    File copying is success. I also allowed to paste the files from user input

--- Backup_Files/backup_logic.py
+++ Backup_Files/backup_logic.py
@@ -11,6 +11,13 @@ class Backup:
         pass
     
     def backup_files(self):
-        timestamp=datetime.datetime.now().strftime("%d-%m-%Y | %H:%M")
-        backup_dir=os.path.join(self.destination_directory,f"backup_file__{timestamp}")
-        
+        timestamp=datetime.datetime.now().strftime("%d-%m-%Y")
+        backup_dir=os.path.join(self.destination_directory,f"backup_file_{timestamp}")
+        # os.makedirs(backup_dir)
+        for root,src,files in os.walk(self.source_directory):
+            for file in files:
+                src_path=os.path.join(root,file)
+                dest_path=os.path.join(backup_dir,os.path.relpath(src_path,self.source_directory))
+                os.makedirs(os.path.dirname(dest_path),exist_ok=True)
+                shutil.copy(src_path,dest_path)
+                print(f"Copied {src_path} to {dest_path}")
--- Backup_Files/main.py
+++ Backup_Files/main.py
@@ -1,9 +1,11 @@
 from backup_logic import Backup
 
 def main():
-    source="C:\Users\Admin\Downloads\Telegram Desktop"
-    destination=""
-    backup=Backup(source_dir=source)
+    # source=r"C:\Users\Admin\Downloads\Telegram Desktop"
+    # destination=r"I:\WorkByVarun\BackupFiles"
+    source =input("Enter the directory which you want to copy: ")
+    destination =input("Enter the directory where you want to paste: ")
+    backup=Backup(source_dir=source,destination_dir=destination)
     backup.backup_files()
     
 if __name__=="__main__":
